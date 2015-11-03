2746c040aa449febdc36918192a0cc0e68cbb3a1
Corbin Murrow <corbin.murrow@gmail.com>
2015-11-03 00:06:55 -0600

    Created first time setup utility...
    Todo: make progress bar work and utility run more slowly.

---
+++ ESA Arduino IDE Configuration Utility/ESA Arduino IDE Configuration Utility/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ESA_Arduino_IDE_Configuration_Utility
+{
+    partial class ConfigUtilityForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.pgbStatus = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(12, 9);
+            this.lblDescription.MaximumSize = new System.Drawing.Size(365, 0);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblDescription.Size = new System.Drawing.Size(364, 39);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "This software installs the BOEbot support libraries, the robot project template f" +
+    "ile, the robot test suite, and performs other configuration operations on the ID" +
+    "E.";
+            // 
+            // pgbStatus
+            // 
+            this.pgbStatus.Location = new System.Drawing.Point(15, 63);
+            this.pgbStatus.Name = "pgbStatus";
+            this.pgbStatus.Size = new System.Drawing.Size(361, 23);
+            this.pgbStatus.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pgbStatus.TabIndex = 1;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 96);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // ConfigUtilityForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 120);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pgbStatus);
+            this.Controls.Add(this.lblDescription);
+            this.Name = "ConfigUtilityForm";
+            this.Text = "ESA Arduino IDE Configuration Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.ProgressBar pgbStatus;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ ESA Arduino IDE Configuration Utility/ESA Arduino IDE Configuration Utility/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ESA_Arduino_IDE_Configuration_Utility
+{
+    public partial class ConfigUtilityForm : Form
+    {
+        public ConfigUtilityForm()
+        {
+            InitializeComponent();
+            Operate();
+        }
+
+        private void Operate()
+        {
+            string appdataRoaming = System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData);
+            string documents = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            string programConfig = Path.Combine(Directory.GetCurrentDirectory(), "config.txt");
+            string boeBotLibraries = Path.Combine(Directory.GetCurrentDirectory(), "BOEbot");
+            string boeBotProjectCopyFrom = Path.Combine(Directory.GetCurrentDirectory(), "ESA_Robot_Project");
+            string boeBotTestCopyFrom = Path.Combine(Directory.GetCurrentDirectory(), "ESA_Robot_Test");
+            string ideConfigFolder = "";
+            string sketchbookFolder = "";
+            string ideConfigLocationOverride = "";
+            string sketchbookLocationOverride = "";
+            bool ideConfigOverride = false;
+            bool sketchbookOverride = false;
+
+            using (StreamReader sr = File.OpenText(programConfig))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null && !(ideConfigOverride && sketchbookOverride))
+                {
+                    if (s[0] != '#')
+                    {
+                        string[] options = s.Split(':');
+                        switch(options[0])
+                        {
+                            case "IDEConfigLocationOverride":
+                                ideConfigOverride = true;
+                                ideConfigLocationOverride = options[1].Trim();
+                                break;
+                            case "SketchbookLocationOverride":
+                                sketchbookOverride = true;
+                                sketchbookLocationOverride = options[1].Trim();
+                                break;
+                            case "IDEConfigFolder":
+                                ideConfigFolder = options[1].Trim();
+                                break;
+                            case "SketchbookFolder":
+                                sketchbookFolder = options[1].Trim();
+                                break;
+                        }
+                    }
+                }
+            }
+
+            string ideConfigPath = ideConfigOverride ? ideConfigLocationOverride : Path.Combine(appdataRoaming, ideConfigFolder);
+            string sketchbookPath = sketchbookOverride ? sketchbookLocationOverride : Path.Combine(documents, sketchbookFolder);
+            string librariesDirectory = Path.Combine(sketchbookPath, @"libraries\BOEbot");
+            string boeBotProject = Path.Combine(sketchbookPath, @"ESA_Robot_Project\ESA_Robot_Project.ino");
+            string boeBotTest = Path.Combine(sketchbookPath, @"ESA_Robot_Test\ESA_Robot_Test.ino");
+
+            lblStatus.Text = "Configuring Arduino IDE...";
+
+            List<String> configData = new List<String>();
+
+            using (StreamReader sr = new StreamReader(ideConfigPath))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    string[] options = s.Split('=');
+                    switch(options[0])
+                    {
+                        case "board": configData.Add("board=lilypad"); break;
+                        case "custom_cpu": configData.Add("custom_cpu=lilypad_atmega328"); break;
+                        case "editor.linenumbers": configData.Add("editor.linenumbers=true"); break;
+                        case "recent.sketches": configData.Add("recent.sketches=" + boeBotProject + "," + boeBotTest);  break;
+                        case "serial.debug_rate": configData.Add("serial.debug_rate=115200"); break;
+                        default: configData.Add(s); break;
+                    }
+                }
+            }
+
+            using (StreamWriter sw = new StreamWriter(ideConfigPath, false))
+            {
+                foreach (string s in configData)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+
+            Microsoft.VisualBasic.Devices.Computer pc = new Microsoft.VisualBasic.Devices.Computer();
+
+            lblStatus.Text = "Installing BOEbot libraries...";
+            pc.FileSystem.CopyDirectory(boeBotLibraries, librariesDirectory, true);
+
+            lblStatus.Text = "Installing Robot Project template...";
+            pc.FileSystem.CopyDirectory(boeBotProjectCopyFrom, Path.Combine(sketchbookPath, "ESA_Robot_Project"), true);
+            pc.FileSystem.CopyDirectory(boeBotTestCopyFrom, Path.Combine(sketchbookPath, "ESA_Robot_Test"), true);
+        }
+    }
+}
